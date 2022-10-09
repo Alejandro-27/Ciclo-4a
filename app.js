1fe11596efc30a57d4512d450637aeb47614bf41
Alejandro-27 <cesaralejandrog67@gamil.com>
2022-10-08 23:00:30 -0500

    new repo

---
+++ app.js
@@ -0,0 +1,18 @@
+function cambiof(){
+    document.getElementById('segunda').src='Screenshot_1.png';
+}
+
+
+//crear un objeto y mostrarlo en el navegador
+console.log({nombre: 'Alejandro', apellido: 'Gomez', edad: 17})
+
+//Crear un objeto dentro de una lista 
+
+var lista = [
+    {nombre: 'Alejandro', apellido: 'Gomez', edad: 17}]
+
+//crear una lista
+
+[1,2,3,4,5];
+
+
